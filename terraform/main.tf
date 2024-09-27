1a6c41c51df871b43b5c25c6fa1b77e686f025a4
Samarth007-2002 <142674758+Samarth007-2002@users.noreply.github.com>
2024-09-27 12:46:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,49 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# VPC Configuration
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "medusa-vpc"
+# Data block to fetch default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Data block to fetch default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "medusa-igw"
-  }
-}
-
-# Subnets in Multiple AZs
-resource "aws_subnet" "subnet_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "medusa-subnet-a"
-  }
-}
-
-resource "aws_subnet" "subnet_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "medusa-subnet-b"
-  }
-}
-
-# Security Group
+# Security Group for ECS
 resource "aws_security_group" "ecs" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = data.aws_vpc.default.id
 
   egress {
     from_port   = 0
@@ -65,94 +38,29 @@ resource "aws_security_group" "ecs" {
   }
 }
 
-# Route Table
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "medusa-route-table"
-  }
-}
-
-# Associate Route Table with Subnets
-resource "aws_route_table_association" "subnet_a" {
-  subnet_id      = aws_subnet.subnet_a.id
-  route_table_id = aws_route_table.main.id
-}
-
-resource "aws_route_table_association" "subnet_b" {
-  subnet_id      = aws_subnet.subnet_b.id
-  route_table_id = aws_route_table.main.id
-}
-
 # ECS Cluster
 resource "aws_ecs_cluster" "main" {
   name = "medusa-cluster"
 }
 
-# ECS Task Definition
-resource "aws_ecs_task_definition" "medusa" {
-  family                   = "medusa-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "1024"
-  memory                   = "3072"
-  execution_role_arn       = aws_iam_role.ecs_execution_role_new.arn
-
-  container_definitions = jsonencode([{
-    name      = "medusa"
-    image     = "${aws_ecr_repository.medusa_new.repository_url}:latest"
-    essential = true
-    portMappings = [{
-      containerPort = 9000
-      hostPort      = 9000
-    }]
-  }])
-}
-
-# ECS Service
+# ECS Service with Fargate Spot
 resource "aws_ecs_service" "medusa" {
   name            = "medusa-service"
   cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.medusa.arn
+  task_definition = "arn:aws:ecs:us-east-1:396913738987:task-definition/medusa-task"
   desired_count   = 1
-  launch_type     = "FARGATE"
+
+  # Define capacity provider strategy for Fargate Spot
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
+  }
+
   network_configuration {
-    subnets          = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+    subnets          = data.aws_subnets.default.ids
     security_groups  = [aws_security_group.ecs.id]
     assign_public_ip = true
   }
-}
-
-# ECR Repository
-resource "aws_ecr_repository" "medusa_new" {
-  name = "medusa-new"
-}
-
-# ECR Repository Policy
-resource "aws_ecr_repository_policy" "medusa_new" {
-  repository = aws_ecr_repository.medusa_new.name
-  policy     = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs.amazonaws.com"
-        },
-        Action = [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetAuthorizationToken"
-        ]
-      }
-    ]
-  })
 }
 
 # IAM Role for ECS Task Execution
